46236b60ec232df304d4b9240f282ad45b341472
Yasin Tatar <ytatar9@googlemail.com>
2023-03-27 01:29:10 +0200

    refactor

--- src/editor.rs
+++ src/editor.rs
@@ -3,32 +3,49 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+
+pub struct Editor {
+    will_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor {}
+        Self { will_quit: false }
     }
 
     /// takes control over stdin/out in raw mode and processes keys
-    pub fn run(&self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
+    pub fn run(&mut self) {
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => die(err),
+        // infinite loop to process key presses until an Error (e.g. Ctrl-Q) comes up
+        loop {
+
+            // if let is like a "match" but for just one specific case
+            if let Err(error) = self.process_keypress() {
+                die(error);
             }
+            if self.will_quit {
+                break;
+            }
+        }
+    }
+
+    fn process_keypress(&mut self) -> Result<(), io::Error> {
+        let key = read_key()?; // propagate the error above
+        match key {
+            Key::Ctrl('q') => self.will_quit = true,
+            _ => ()
+        }
+
+        Ok(()) // return empty result for now
+    }
+}
+
+// utility function
+fn read_key() -> Result<Key, io::Error> {
+    loop {
+        // loop until stdio returns a key (Option - can be None or Some)
+        if let Some(k) = io::stdin().lock().keys().next() {
+            return k;
         }
     }
 }
